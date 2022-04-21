091f64a73ce021c43e7196b8aa00736f498ac574
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-20 21:02:49 -0400

    App js Changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Components
+import Home from './components/Home'
+import BookFlight from './components/BookFlight'
+import Reservation from './components/Reservation'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/book-flight' element={<BookFlight />} />
+          <Route path='/reservation' element={<Reservation />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
