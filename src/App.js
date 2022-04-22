686635696e1ac7ef09227b82e2ffc06681e68380
Otto Jones <ottosjonesjr@gmail.com>
2022-04-21 21:16:51 -0400

    added reservation page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 // Components
 import Home from './components/Home'
@@ -15,14 +15,14 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/book-flight' element={<BookFlight />} />
-          <Route path='/reservation' element={<Reservation />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/book-flight" element={<BookFlight />} />
+          <Route path="/reservation/:id" element={<Reservation />} />
         </Routes>
         <Footer />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function Reservation() {
+  const [data, setData] = useState('')
+
+  const reservationId = useParams()
+  console.log(reservationId.id)
+
+  const API_URL = `http://localhost:3000/customers/reservations/${reservationId.id}`
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(API_URL)
+      const resData = await response.json()
+      // console.log(resData)
+      console.log(resData.flightNumberId)
+      console.log(resData.seatNumber)
+      console.log(resData.passenger)
+      console.log(resData)
+      setData(resData)
+    }
+    fetchData()
+  }, [reservationId.id])
+
+  function upperCase(word) {
+    let upper = word.charAt(0).toUpperCase() + word.slice(1)
+    return upper
+  }
   return (
-    <div>Reservation</div>
+    <>
+      <h4 className="mt-5">Reservation Number</h4>
+      <h6>{!data ? null : data._id}</h6>
+      <main className="container">
+        <h5 className="mt-5">Flight Infomation</h5>
+        <div className="row g-3 mt-3">
+          <p className="col-12">
+            {!data
+              ? null
+              : `Flight Number - ${data.flightNumberId.flightNumber}`}
+          </p>
+          <p className="col-6">
+            Departing From - {!data ? null : data.flightNumberId.departure}
+          </p>
+          <p className="col-6">
+            Arriving At - {!data ? null : data.flightNumberId.destination}
+          </p>
+          <p className="col-6">
+            Depature Date -{' '}
+            {!data ? null : data.flightNumberId.departureDate.slice(0, 7)}
+          </p>
+          <p className="col-6">
+            Arrival Date -{' '}
+            {!data ? null : data.flightNumberId.arrivalDate.slice(0, 7)}
+          </p>
+          <p className="col-6">
+            Depature Time - {!data ? null : data.flightNumberId.departureTime}
+          </p>
+          <p className="col-6">
+            Arrival Time - {!data ? null : data.flightNumberId.arrivalTime}
+          </p>
+          {/* <p className="col-6">
+            Seat Number - {!data ? null : data.seatNumber.seatnumber}
+          </p>
+          <p className="col-6">
+            Seat Class - {!data ? null : upperCase(data.seatNumber.seatclass)}
+          </p> */}
+        </div>
+      </main>
+    </>
   )
 }
 
