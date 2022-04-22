f381ae7778fff955fe4a7d4e08232bf124ce0071
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-21 22:25:41 -0400

    HomePage with searchbar and Content Working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+// Components
+import SearchFlight from './subcomponents/SearchFlight'
+import Content from './subcomponents/Content'
 
 function Home() {
+  const [flights, setFlights] = useState('')
   return (
-    <div>Home</div>
+    <Container className='mt-5'>
+      <Row className='border'>
+        <Col xs={12}>
+          <h3 className='mt-2'>Search Flight</h3>
+          
+          <SearchFlight setFlights={setFlights} flights={flights} />
+        </Col>
+      </Row>
+      <Row className='mt-5'>
+        <Col xs={12}>
+          <Content />
+        </Col>
+      </Row>
+    </Container>
+
   )
 }
 
