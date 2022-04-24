e8ebdcd36f60b621dc6afa5107a00bef8fed278e
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-24 09:37:01 -0400

    Added alert when no flights found

--- src/components/subcomponents/SearchFlight.js
+++ src/components/subcomponents/SearchFlight.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 //import context
 import { GlobalContext } from '../helpers/GlobalContext'
@@ -19,6 +20,7 @@ function SearchFlight() {
   const [destination, setDestination] = useState('')
   const [departureDate, setDepartureDate] = useState('')
   const [numberOfSeats, setNumberOfSeats] = useState('')
+  const [flightNotFound, setFlightNotFound] = useState(false)
 
   const getData = async () => {
         const response = await fetch(`http://localhost:3000/customers/search?departure=${departure.toUpperCase()}&destination=${destination.toUpperCase()}&departureDate=${departureDate}&numberOfSeats=${numberOfSeats}`)
@@ -26,9 +28,11 @@ function SearchFlight() {
         if(fetchedFlights.flights){
           setFlights(fetchedFlights.flights)
           navigate('/results')
+          return
         }
+        setFlightNotFound(true)
+        setTimeout(() => setFlightNotFound(false), 2500)
       }
-      // RDU, JFK, 2022-04-30, 5
 
   return (
     <Form className='mt-3' onSubmit={ (e)=>{
@@ -36,6 +40,7 @@ function SearchFlight() {
       getData()
     }}>
       <Form.Group controlId='search-flight'>
+        {flightNotFound ? <Alert variant="danger">No Flights Found!</Alert> : null}
         <Row>
           <Col sm={6} md={3} className='my-3' >
             <FloatingLabel controlId='departure' label='Leaving from'>
@@ -55,7 +60,7 @@ function SearchFlight() {
           </Col>
           <Col sm={6} md={3} className='my-3'>
             <FloatingLabel controlId='number-of-seats' label='Travelers:'>
-              <Form.Control type='number' min='1' max='6' placeholder='1' inputMode="numeric"   onChange={(e)=> { setNumberOfSeats(e.target.value) }} required/>
+              <Form.Control type='number' min='1' max='10' placeholder='1' inputMode="numeric"   onChange={(e)=> { setNumberOfSeats(e.target.value) }} required/>
             </FloatingLabel>
           </Col>
         </Row>
