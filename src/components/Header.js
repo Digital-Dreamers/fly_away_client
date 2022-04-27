9ecc691b041736da39c776fafe1ab94b257b3dab
Miguel(MgARL) <mgarlcr@gmail.com>
2022-04-26 21:38:46 -0400

    Removed Unused Elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,39 +7,28 @@ function Header() {
     const [reservationNumber, setReservationNumber] = useState('')
 
     return (
-        <div bg="dark" variant="dark" fixed="top">
-            <>
+        
                 <Navbar bg="primary" variant="dark">
-                    <Container>
+                    <Container className="border">
                         <Navbar.Brand as={Link} to="/">Fly Away</Navbar.Brand>
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
-                          
-                            <div className="search-container">
-                              
+                            <div className="search-container">                     
                                 <Form className="d-flex" onSubmit={(e) => {
                                     e.preventDefault()
                                     // navigate to the page of reservation           
                                     navigate(`/reservation/${reservationNumber}`)
-
                                     setReservationNumber('')
                                 }}>
-                                  
                                     <Form.Control type="search" value={reservationNumber} placeholder="Enter Reservation"
                                         className="justify-content-end"
                                         onChange={(e) =>  setReservationNumber(e.target.value)} />
                                     <Button variant="success" type='submit'><i className="fa fa-search" aria-hidden="true"></i></Button>
-                                    {/* </Form> */}
                                 </Form>
                             </div>
                         </Nav>
                     </Container>
                 </Navbar>
-
-
-
-            </>
-        </div>
     )
 }
 export default Header
